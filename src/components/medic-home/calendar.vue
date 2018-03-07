d460554c80cc35c92dbb01b99013abaab12e13c2
Taylor Tobin <19tt94@gmail.com>
2018-03-06 18:48:51 -0800

    up to date calendar with calendar helper

--- src/components/medic-home/calendar.vue
+++ src/components/medic-home/calendar.vue
@@ -1,42 +1,41 @@
 <template>
 
-
   <div class="calendar-wrapper">
     <div class="calendar">
 
       <div class="columns is-mobile calendar__menu">
         <div class="item"><div class="calendar__menu--arrow-left" @click="previous"></div></div>
-        <div class="item calendar__menu--label current-month"><h1>{{getCurrent().monthName}}</h1></div>
-        <div class="item calendar__menu--label current-week"><h1>Week of the {{getCurrent().monday}}th</h1></div>
+        <div class="item calendar__menu--label current-month"><h1>{{months[calendar[15].month]}}</h1></div>
+        <div class="item calendar__menu--label current-week"><h1>Week of the {{getCurrent.monday}}th</h1></div>
         <div class="item"><div class="calendar__menu--arrow-right" @click="next"></div></div>
 
-        <div class="item calendar__menu--button active" @click="month"><h1 class="text">Month</h1></div>
-        <div class="item calendar__menu--button" @click="week"><h1 class="text">Week</h1></div>
+        <div class="item calendar__menu--button" @click="monthly"><h1 class="text">Month</h1></div>
+        <div class="item calendar__menu--button active" @click="weekly"><h1 class="text">Week</h1></div>
       </div>
 
       <div class="columns is-multiline monthly">
         <div class="column is-one-fifth calendar__day"
-          v-for="day, index in getMonth().length"
+          v-for="day, index in calendar.length"
           :class="{
             'no-right' : (index+1)%5 === 0,
             'no-bottom': (index > 19),
-            'weekly': getMonth()[index].date < getCurrent().monday || getMonth()[index].date > getCurrent().friday,
-            'no-bottom__mobile': getMonth()[index].date === getCurrent().friday
+            'weekly': calendar[index].date < getCurrent.monday || calendar[index].date > getCurrent.friday,
+            'no-bottom__mobile': calendar[index].date === getCurrent.friday
           }">
 
           <div class="calendar__day--date"
             :class="{
-                'today' : getCurrent().date === getMonth()[index].date
-          }">{{getMonth()[index].date}}</div>
+                'today' : getCurrent.date === calendar[index].date
+          }">{{calendar[index].date}}</div>
 
           <div class="blocked"
-            v-if="getMonth()[index].month != getCurrent().month"
+            v-if="calendar[index].month != getCurrent   .month"
             :class="{
               'rounded-left': (index === 0),
               'rounded-right': (index === 24),
           }"></div>
 
-          <div class="calendar__day--label" v-if="index < 5">{{getMonth()[index].name}}</div>
+          <div class="calendar__day--label" v-if="index < 5">{{calendar[index].name}}</div>
         </div>
 
       </div>
@@ -47,82 +46,27 @@
 </template>
 
 <script>
-
 export default {
   name: 'app',
-  components: {},
+  created: function() {
+      this.calendar = this.$renderCalendar(0);
+  },
+  data() {
+    return {
+      months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      week: ["Sun","Mon", "Tue", "Wed", "Thu", "Fri","Sat"],
+      calendar: []
+    }
+  },
+  // use vue.set to update array (Vue.$set)
   methods: {
-    getCurrent: function() {
-      // get today's date object
-      let current = new Date();
-      // get current monday
-      let monday = new Date(current.getFullYear(), current.getMonth(), current.getDate() + (current.getDay() == 0?-6:1) - current.getDay());
-      //get current friday
-      let friday = new Date(monday.getFullYear(), monday.getMonth(), monday.getDate() + 4);
-      // month array
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      return {
-        "date": current.getDate(),
-        "day": current.getDay(),
-        "month": current.getMonth(),
-        "monthName": months[current.getMonth()],
-        "year": current.getFullYear(),
-        "monday": monday.getDate(),
-        "friday": friday.getDate()
-      };
+    next: function(event) {
+      this.calendar = this.$renderCalendar(1);
     },
-    getMonth: function(start) {
-      // week day array
-      let week = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri","Sat"];
-      // get today's date object
-      let today = new Date();
-      // get the first day of the month
-      if(start === 1) {
-        start = new Date(today.getFullYear(), today.getMonth() + 1, 1);
-      } else if(start === 0) {
-        start = new Date(today.getFullYear(), today.getMonth() - 1, 1);
-      } else {
-        start = new Date(today.getFullYear(), today.getMonth(), 1);
-      }
-
-      // start on monday
-      start = new Date(start.getFullYear(), start.getMonth(), start.getDate() + (start.getDay() == 0?-6:1) - start.getDay());
-
-      // careaway calendar month
-      let month = [];
-
-      let next = 0, count = 0;
-      while(count < 25) {
-        //skip saturdays and sundays
-        if(start.getDay() === 0) {
-          next = 1; //set next after first itr
-          start.setDate(start.getDate() + next);
-        } else if(start.getDay() === 6) {
-          next = 1; //set next after first itr
-          start.setDate(start.getDate() + next + next);
-        }
-
-        // add day object to month
-        month[count] = {
-          "object": start,
-          "date": start.getDate(),
-          "code": start.getDay(),
-          "month": start.getMonth(),
-          "name": week[start.getDay()]
-        }
-
-        // update count & set next after first itr
-        count++;
-        next = 1;
-
-        // increment date
-        start.setDate(start.getDate() + next);
-      }
-
-      // return array of day objects
-      return month;
+    previous: function(event) {
+      this.calendar = this.$renderCalendar(-1);
     },
-    week: function(event) {
+    weekly: function(event) {
       let days = document.getElementsByClassName("monthly")[0].children;
       Array.from(days).forEach((item)=> {
         if(item.classList.contains("weekly"))
@@ -134,7 +78,7 @@ export default {
       document.getElementsByClassName("calendar__menu--button")[0].classList.add("active");
       document.getElementsByClassName("calendar__menu--button")[1].classList.remove("active");
     },
-    month: function(event) {
+    monthly: function(event) {
         let days = document.getElementsByClassName("monthly")[0].children;
         Array.from(days).forEach((item)=> {
           if(item.classList.contains("weekly"))
@@ -145,19 +89,33 @@ export default {
 
         document.getElementsByClassName("calendar__menu--button")[1].classList.add("active");
         document.getElementsByClassName("calendar__menu--button")[0].classList.remove("active");
-    },
-    next: function(event) {
-      console.log("next");
-    },
-    previous: function(event) {
-      console.log("previous");
-    },
+    }
+  },
+
+  computed: {
+    getCurrent: function() {
+      // get today's date object
+      let current = new Date();
+      // get current monday
+      let monday = new Date(current.getFullYear(), current.getMonth(), current.getDate() + (current.getDay() == 0?-6:1) - current.getDay());
+      //get current friday
+      let friday = new Date(monday.getFullYear(), monday.getMonth(), monday.getDate() + 4);
+      return {
+        "date": current.getDate(),
+        "day": current.getDay(),
+        "month": current.getMonth(),
+        "year": current.getFullYear(),
+        "monday": monday.getDate(),
+        "friday": friday.getDate()
+      };
+    }
   }
 }
 </script>
 
 <style lang="scss">
-@import "../../assets/sass/settings.scss";
+
+@import '../../assets/sass/settings.scss';
 
 .weekly {
   display: none;
@@ -226,12 +184,11 @@ export default {
       @media #{$tablet} {
         display: block;
       }
-      
-      background-color: $white;
+
+      background-color: $green;
       padding: 0 20px;
       text-align: center;
       border: 2px solid $green;
-      background: $green;
       border-top-left-radius: 10px;
       border-top-right-radius: 10px;
 
@@ -317,7 +274,7 @@ export default {
 }
 
 .active {
-  background: none;
+  background-color: $white;
 }
 
 .week-height {
