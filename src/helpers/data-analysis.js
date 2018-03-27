c7598a700e56fa77a1b7c8c5e5c5c38b7502bf2e
Felix <felixthe8@gmail.com>
2018-03-27 12:38:39 -0700

    Fixed coding standard.

--- src/helpers/data-analysis.js
+++ src/helpers/data-analysis.js
@@ -4,99 +4,97 @@ import Chart from 'chart.js';
 const Report = {};
 
 Report.install = function(Vue, options) {
-    // Generate the dates of the past Monday - Friday
-    Vue.prototype.$generateDays = function () {
-        var days = [];
-        for (var i = 0; i <= 4; i++ ) {
-            var singleDay = moment().day(-2).subtract(i,'days').format("YYYY-MM-DD");
-            days.unshift(singleDay);
-        }
-        return days;
+  // Generate the dates of the past Monday - Friday
+  Vue.prototype.$generateDays = function () {
+    var days = [];
+    for (var i = 0; i <= 4; i++ ) {
+      var singleDay = moment().day(-2).subtract(i,'days').format("YYYY-MM-DD");
+      days.unshift(singleDay);
     }
+      return days;
+  }
 
     Vue.prototype.$emptyBar = function(id, days) {
-        // Generate an empty bar graph
-        new Chart(document.getElementById(id), {
-            type: 'bar',
-            data: {
-              labels: days,
+    // Generate an empty bar graph
+    new Chart(document.getElementById(id), {
+      type: 'bar',
+      data: {
+        labels: days,
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: true,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              suggestedMax: 100
             },
-            options: {
-                responsive: false,
-                maintainAspectRatio: true,
-                scales: {
-                    yAxes: [{
-                      ticks: {
-                        beginAtZero: true,
-                        suggestedMax: 100
-                      },
-                     
-                    }]
-                  },
-                }   
-        })
-    }
+          }]
+        },
+      }   
+    })
+  }
 
     Vue.prototype.$emptyDoughnut = function(id) {
-        // Generate an empty doughnut graph with no data
-        new Chart(document.getElementById(id), {
-            type: 'doughnut',
-            data: {
-                labels: ["No Patients"],
-                datasets: [{
-                    data: [1],
-                }]
-            },
-            options: {
-                responsive: false,
-                maintainAspectRatio: true,
-                legend: {
-                    display: true,
-                    position: "left",
-                    labels: {fontSize: 14},
-                    // By default Chart JS removes data when you click it on the legend. Override the default action so it does nothing. 
-                    onClick: null
-                  },
-            }
-        })
+      // Generate an empty doughnut graph with no data
+      new Chart(document.getElementById(id), {
+        type: 'doughnut',
+        data: {
+          labels: ["No Patients"],
+          datasets: [{
+            data: [1],
+          }]
+        },
+        options: {
+          responsive: false,
+          maintainAspectRatio: true,
+          legend: {
+            display: true,
+            position: "left",
+            labels: {fontSize: 14},
+            // By default Chart JS removes data when you click it on the legend. Override the default action so it does nothing. 
+            onClick: null
+          },
+        }
+      })
     }
 
     Vue.prototype.$getTrends = function(data) {
-         // Create 2 arrays - 1 for holding the positive trends and 1 for holding the negative trends
-        var positiveTrends = [], negativeTrends = [];
-        var startIndex = 0;
-        // Determine the positive trends first. Loop through the data
-        for(var i = 0; i < data.length; i++) {
-            // If the data at one index is less than the one at the next, this is the start of a positive trend
-            if(data[i] <= data[i+1]) {
-            continue;
-            } else {
-            // When the if condition fails, push the starting index and ending index of the positive trend into an array  
-            positiveTrends.push({
-            start: startIndex, 
-            end: i
-          })
-          // Set the new starting index
-          startIndex = i + 1
+      // Create 2 arrays - 1 for holding the positive trends and 1 for holding the negative trends
+      var positiveTrends = [], negativeTrends = [];
+      var startIndex = 0;
+      // Determine the positive trends first. Loop through the data
+      for(var i = 0; i < data.length; i++) {
+        // If the data at one index is less than the one at the next, this is the start of a positive trend
+        if(data[i] <= data[i+1]) {
+          continue;
+        } else {
+          // When the if condition fails, push the starting index and ending index of the positive trend into an array  
+          positiveTrends.push({
+          start: startIndex, 
+          end: i
+        })
+        // Set the new starting index
+        startIndex = i + 1
         }
       }
       // Remove the instances where start and end values are the same. 
       positiveTrends = positiveTrends.filter(trend => trend.start != trend.end);
-      
       // Determine the negative trends next. Reset the startIndex variable to 0. 
       startIndex = 0;
       for(var i = 0; i < data.length; i++) {
-            // If the data at one index is greater than the one at the next, this is the start of a negative trend. 
-            if(data[i] >= data[i+1]) {
-              continue;
-            } else {
-              // When the condition fails, pushing the starting index and ending index of the negative trend to the array
-              negativeTrends.push({
-                start: startIndex, 
-                end: i
-              })
-              startIndex = i + 1
-            } 
+        // If the data at one index is greater than the one at the next, this is the start of a negative trend. 
+        if(data[i] >= data[i+1]) {
+          continue;
+        } else {
+          // When the condition fails, pushing the starting index and ending index of the negative trend to the array
+          negativeTrends.push({
+            start: startIndex, 
+            end: i
+          })
+          startIndex = i + 1
+        } 
       }
       // Remove the instances where start and end values are the same. 
       negativeTrends = negativeTrends.filter(trend => trend.start != trend.end);
--- src/helpers/user.js
+++ src/helpers/user.js
@@ -4,24 +4,24 @@ import store from '../store/store';
 const User = {};
 User.install = function(Vue, options) {
     Vue.prototype.$logout = function() {
-        // Clears the authenticated username 
-        this.$store.dispatch('deauthenticatedUsername', '');
-        // Clears the medical code
-        this.$store.dispatch('medicalCode', '');
-        // Clears the authenticated role
-        this.$store.dispatch('signOut', '');
+      // Clears the authenticated username 
+      this.$store.dispatch('deauthenticatedUsername', '');
+      // Clears the medical code
+      this.$store.dispatch('medicalCode', '');
+      // Clears the authenticated role
+      this.$store.dispatch('signOut', '');
     }
 
     Vue.prototype.$login = function(accountType, userName) {
-        // Sets the authenticated user role
-        this.$store.dispatch('signIn', accountType);
-        // Sets the authenticated username
-        this.$store.dispatch('authenticatedUsername', userName);
+      // Sets the authenticated user role
+      this.$store.dispatch('signIn', accountType);
+      // Sets the authenticated username
+      this.$store.dispatch('authenticatedUsername', userName);
     }
 
     Vue.prototype.$regCheck = function(inputFields) {
-        // Checks to see if each element in the array has a value of 'is-success'
-        return inputFields.every((field) => {return field == 'is-success'})
+      // Checks to see if each element in the array has a value of 'is-success'
+      return inputFields.every((field) => {return field == 'is-success'})
     }
 }
 
