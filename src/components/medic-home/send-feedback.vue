8eae670fd98d4089d967f4890a0c5d3ac5a8c8e6
lmao <fireclimbers@gmail.com>
2018-04-17 12:23:40 -0700

    added v-model and cwick bindings to make the componyant more ~vuey~ and an axiows post call to the send method!!

--- src/components/medic-home/send-feedback.vue
+++ src/components/medic-home/send-feedback.vue
@@ -4,9 +4,9 @@
     Do you have any questions, comments, or concerns? Have any <span style="font-weight:bold">cool</span> ideas to improve Careaway?
     You can write down those thoughts in the text box below, send it, and the Careaway team will take your message into consideration when making updates to Careaway.
     <br/><br/>
-    <textarea class="textarea" placeholder="Enter text"></textarea>
+    <textarea v-model="feedbackText" class="textarea" placeholder="Enter text"></textarea>
     <br/>
-    <a class="button is-primary">Send</a>
+    <a class="button is-primary" @click="sendFeedbackCall">Send</a>
   </div>
 </template>
 
@@ -15,8 +15,26 @@ import axios from 'axios';
 
 export default {
   name: 'sendFeedback',
+  data() {
+    return {
+      feedbackText: '',
+      sendSuccessful: false,
+      errorMessage: ''
+    }
+  },
   methods: {
-    
+    sendFeedbackCall() {
+      var feedbackObj = {feedback: this.feedbackText};
+      axios.post(/*this.$store.getters.something*/'placeholder', feedbackObj)
+        .then(response => {
+          if(response.data.success) {
+            this.errorMessage = '';
+            this.sendSuccessful = true;
+          } else {
+            this.errorMessage = response.data.error;
+          }
+        });
+    }
   },
   created(){
       //this.$store.dispatch("singlePatientWellness",null);
