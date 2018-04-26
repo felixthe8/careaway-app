1962ec51ecd5ad2e79e38e4f1aa2de17f458c6e4
Taylor Tobin <19tt94@gmail.com>
2018-04-25 22:16:32 -0700

    added date and setup for remove

--- src/components/shared/mail.vue
+++ src/components/shared/mail.vue
@@ -7,7 +7,10 @@
       </button>
 
       <div class="mail__menu" :class="{ 'show-mail': open }">
-        <div class="mail__menu--current-message" v-for="message in mail">{{message.message}}</div>
+        <div class="mail__menu--current-message" v-for="message in mail">
+            <div class="mail__menu--current-message--date">{{date.toString()}}</div>
+            {{message.message}}<button class="message-delete" @click="remove"><i class="fas fa-times"></i></button>
+        </div>
         <hr class="line">
         <input class="input" name="message" type="text" id="message">
         <button id="meter" class="mail__menu--create green-button" @click="create">Send</button>
@@ -94,13 +97,22 @@ export default {
         'message' : this.message
       }).then(function(response) {
         if(response.data.success) {
-          // success
+          alert("Message sent!")
         } else {
           alert("Failed to Send Message");
         }
       }).catch(function(err) {
           throw err;
       });
+    },
+    remove: function() {
+        console.log("remove");
+    }
+  },
+
+  computed: {
+    date: function() {
+      return new Date();
     }
   }
 }
@@ -134,23 +146,39 @@ export default {
   }
 
   &__menu {
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      margin: 3rem;
-      padding: 1rem;
-      border: 2px solid $green;
-      border-radius: 10px;
-      background: $white;
-      box-shadow: 0px 2px 5px rgba(0,0,0,0.2);
-      transform: scale(0) translateY(500px);
-      opacity: 0;
-      transition: all ease 1s;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin: 3rem;
+    padding: 1rem;
+    border: 2px solid $green;
+    border-radius: 10px;
+    background: $white;
+    box-shadow: 0px 2px 5px rgba(0,0,0,0.2);
+    transform: scale(0) translateY(500px);
+    opacity: 0;
+    transition: all ease 1s;
 
-      &--current-message {
-        text-align: center;
-        color: gray;
+    &--current-message {
+      text-align: left;
+      color: gray;
+      max-height: 600px;
+
+      &:hover {
+        .message-delete {
+          opacity: 1;
+        }
       }
+
+      .message-delete {
+        opacity: 0;
+      }
+
+      &--date {
+        color: $green;
+        font-size: 11px;
+      }
+    }
   }
 }
 
