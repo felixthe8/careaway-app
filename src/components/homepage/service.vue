8e7fe92ffabaaf614a492774b267f59ded8a7fbf
Felix <felixthe8@gmail.com>
2018-04-25 22:34:03 -0700

    Updated icons on the homepage.

---
+++ src/components/homepage/service.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class = "icon">
+    <i class = "fa-fw fa-3x" :class = "icon"></i>
+  </span>
+  
+</template>
+
+
+<script>
+export default {
+    name: 'service',
+    props: ['icon']
+  
+}
+</script>
