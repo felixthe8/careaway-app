f635bedf67a9a447d476a9f1aae11ceab35bba5c
Taylor Tobin <19tt94@gmail.com>
2018-04-27 16:23:52 -0700

    fixed styles in device block

--- src/components/homepage/device-block.vue
+++ src/components/homepage/device-block.vue
@@ -3,18 +3,20 @@
   <section class="section device-block">
     <div class="wrapper" v-for="section in sections" >
       <div class="columns" v-if="section.default === true">
-        <div class="column device-block__description">
-          <p class="device-block__description--text">{{section.description}}</p>
-        </div>
-        <div class="column">
-          <img class="device-block__device" :src="section.device">
+        <div class="reverse">
+          <div class="column device-block__description reverse-description">
+            <p class="device-block__description--text">{{section.description}}</p>
+          </div>
+          <div class="column">
+            <img class="device-block__device" :src="section.device">
+          </div>
         </div>
       </div>
       <div class="columns" v-else>
-        <div class="column">
+        <div class="column reverse">
           <img class="device-block__device" :src="section.device">
         </div>
-        <div class="column device-block__description">
+        <div class="column device-block__description  reverse">
           <p class="device-block__description--text">{{section.description}}</p>
         </div>
       </div>
@@ -63,22 +65,45 @@
   background: $green-light;
 
   &__device {
-      width: 300px;
-      height: 300px;
-      filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.4));
+    display: flex;
+    width: 300px;
+    height: 300px;
+    filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.4));
+    margin: 0 auto;
+
+    @media #{$tablet} {
+      width: 550px;
+      margin: auto;
+    }
   }
 
   &__description {
-      text-align: center;
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-      &--text {
-          max-width: 50%;
-      }
+    &--text {
+      max-width: 50%;
+    }
   }
 
+}
+
+.reverse {
+  display: flex;
+  flex-direction: column-reverse;
+  padding: 1rem;
+
+  @media #{$tablet} {
+    display: flex;
+    flex-direction: row;
+    padding-bottom: auto;
+  }
+}
+
+.reverse-description {
+    padding: 5rem 0;
 }
 
 </style>
